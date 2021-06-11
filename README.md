4abefb4182bf5f78f72d84afc47d9297f4a7e114
Henry Jr <hmjsite@gmail.com>
2021-06-10 22:22:39 -0300

    Batman

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# wpfilegator
+## WPFilegator
 Add smart and secure WebFTP to WordPress.
+Account default.
+- `admin/admin123`
+
+## Filegator Project
+Site [https://filegator.io](https://filegator.io)
+Git x[https://github.com/filegator/filegator](https://github.com/filegator/filegator)
+
+## Demo
+[https://demo.filegator.io](https://demo.filegator.io)
+
+This is read-only demo with guest account enabled.
+- you can log in as `john/john` to see John's private files
+- or `jane/jane` as readonly + download user.
+
+## Documentation
+[Check out the documentation](https://docs.filegator.io/)
+
+## Minimum Requirements
+- PHP 7.2+ (with php-zip extension)
+
+See [install instructinos](https://docs.filegator.io/install.html) for Ubuntu 18.04 or Debian 10.3. Get $100 in ([server credits here](https://m.do.co/c/93994ebda78d)) so you can play around.
---
+++ index.php
@@ -0,0 +1,39 @@
+<?php
+/*
+Plugin Name: WPFilegator
+Plugin URI: https://t.me/WordPressBrasil
+description: Add smart and secure WebFTP to WordPress.
+Version: 0.1
+Author: @henryjr
+Author URI: https://t.me/WordPressBrasil
+License: Que licença?
+*/
+
+add_action('admin_menu', 'wpfilegator');
+function wpfilegator()
+{
+    add_options_page(
+        'WPFilegator',
+        'WPFilegator',
+        'manage_options',
+        'wpfilegator-options',
+        'wpfilegator_content'
+    );
+}
+
+function wpfilegator_content()
+{ ?>
+    <div class="wrap">
+        <div id="dashboard_site_health" class="postbox">
+            <div class="postbox-header">
+                <h2 class="hndle ui-sortable-handle" style="padding-left: 30px">WPFilegator - WebFTP</h2>
+            </div>
+            <div class="inside">
+                <iframe src="/wp-content/plugins/WPFilegator/filegator/index.php"
+                        style="width:100%;height:700px;border:none;overflow:hidden;z-index:999999;">
+                    Your browser doesn't support iframes
+                </iframe>
+            </div>
+        </div>
+    </div>
+<?php }
